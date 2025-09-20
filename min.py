36a58b494f3bbae50353716acae77c6917d415ab
Austen9 <austendgreat3@gmail.com>
2025-09-20 03:47:19 +0000

    Closing Edit

--- min.py
+++ min.py
@@ -15,26 +15,32 @@ active = True
 while active:
 
     if ageconfirm == 'Y':
-        menusel = input("how may i assist you today? \n 1. how old will i be in 5 years \n 2. how many letters are within my name  " )
+        menusel = input("how may i assist you today? \n 1. how old will i be in 5 years \n 2. how many letters are within my name \n 3. Exit  " )
         if menusel == '1':
             agecalc = int(age) + 5
             print(f"you'll be "+str(agecalc)+" in 5 years." )
             active = False
-        else:
+        elif menusel == '2':
             lcount=len(name)
             print(lcount)
-            active = False    
+            active = False 
+        elif menusel == '3':
+            print('Goodbye!')
+            break
+
     elif ageconfirm == 'N':
 
         age,ageconfirm = ageconfirmer()
         if ageconfirm == 'Y':
-             menusel = input("how may i assist you today? \n 1. how old will i be in 5 years \n 2. how many letters are within my name  " )
-        if menusel == '1':
-            agecalc = int(age) + 5
-            print(f"you'll be "+str(agecalc)+" in 5 years." )
-            active = False
-        else:
-            lcount=len(name)
-            print(lcount)
-            active = False 
-        
+            menusel = input("how may i assist you today? \n 1. how old will i be in 5 years \n 2. how many letters are within my name \n 3. Exit  " )
+            if menusel == '1':
+                agecalc = int(age) + 5
+                print(f"you'll be "+str(agecalc)+" in 5 years." )
+                active = False
+            elif menusel == '2':
+                lcount=len(name)
+                print(lcount)
+                active = False 
+            elif menusel == '3':
+                print('Goodbye!')
+                break
